d6048c1479d2d975bc0f94ed359eb99ac44e4907
Rajesh tata <89087118+Rajeshtata@users.noreply.github.com>
2023-02-28 11:25:38 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
 import streamlit as st
-st.header("Rajaenv")
+import os
+import base64
+
+st.set_page_config(page_title='My Portfolio ')
+
+st.title(":blue[Rajesh Tata] :two_hearts:")
+st.subheader("Intern at Innomatics Research Lab....")
+st.write("I am a data scientist Intern and my main objective is to learn new skills and looking for an opportunity as Data Scientist where I can use my knowledge and experience in statistics, machine learning and Deep Learning... ")
+st.snow()
+
+st.subheader('Contact')
+st.write('Email: thatharajesh431@gmail.com')
+st.write("Ph-Number : 9985829191")
+btn=st.button("Social media")
+if btn == True:
+    st.write('LinkedIn: https://www.linkedin.com/in/rajesh-tata-87447b224/ ')
+    st.write('Instagram: https://www.instagram.com/its_rajesh_thatha/?next=%2F')
+    st.success('Successfully completed', icon="✅")
+    st.balloons()
